6d82d9178153d1c3adba679744652ce636fe04b9
Daniel Pakuschewski <contato@danielpk.com.br>
2014-07-31 17:29:53 -0300

    Fixed error when user close window after payment complete on MP.

--- app/controllers/spree/mercado_pago_controller.rb
+++ app/controllers/spree/mercado_pago_controller.rb
@@ -2,15 +2,16 @@ module Spree
   class MercadoPagoController < StoreController
 
     def ipn
-      notification = gateway.notification(params[:id])
+      if params[:topic] == 'payment'
+        notification = gateway.notification(params[:id])
+        external_reference = notification["collection"]["external_reference"]
 
-      external_reference = notification["collection"]["external_reference"]
+        order_no = external_reference.split('@').first
+        order_status = notification["collection"]["status"]
+        order = Spree::Order.find_by(number: order_no)
 
-      order_no = external_reference.split('@').first
-      order_status = notification["collection"]["status"]
-      order = Spree::Order.find_by(number: order_no)
-
-      update_payment_status(order, order_status)
+        update_payment_status(order, order_status)
+      end
 
       render text: "OK"
     end
@@ -29,60 +30,57 @@ module Spree
     end
 
     private
-      def update_payment_status(order, order_status)
-        raise ActionController::RoutingError.new('Not Found') unless order.payments.any?
+    def update_payment_status(order, order_status)
+      order = create_payment_for(order)
 
-        payment = order.payments.last
-
-        case order_status
-        when 'approved'
-          payment.complete
-        when 'pending', 'in_process', 'rejected'
-          payment.pend
-        when 'refunded', 'cancelled'
-          payment.failure
-        end
+      unless order.complete?
+        order.state = 'complete'
+        order.save
       end
 
-      def gateway
-        mercado_pago_gateway || create_mercado_pago_gateway
+      payment = order.payments.last
+
+      case order_status
+      when 'approved'
+        payment.complete
+      when 'pending', 'in_process', 'rejected'
+        payment.pend
+      when 'refunded', 'cancelled'
+        payment.failure
+      end
+    end
+
+    def gateway
+      Spree::PaymentMethod.where(type: 'Spree::Gateway::MercadoPago', environment: Rails.env).first
+    end
+
+    def proccess_order
+      order = Spree::Order.by_number(params["order"]).first
+      raise(ActiveRecord::RecordNotFound) if order.nil?
+
+      order = create_payment_for(order)
+
+      if order.payment?
+        order.state = 'complete'
+        order.save
       end
 
-      def create_mercado_pago_gateway
-        Spree::PaymentMethod.create({
-          type: 'Spree::Gateway::MercadoPago',
-          environment: Rails.env,
-          name: 'Mercado Pago',
-          active: true
+      if order.complete?
+        flash[:success] = Spree.t(:order_mp_processed_successfully)
+        redirect_to order_path(order, token: order.token)
+      else
+        redirect_to checkout_state_path(order.state)
+      end
+    end
+
+    def create_payment_for(order)
+      unless order.payments.any?
+        order.payments.create!({
+          amount: order.total,
+          payment_method: gateway
         })
       end
-
-      def mercado_pago_gateway
-        Spree::PaymentMethod.where(type: 'Spree::Gateway::MercadoPago', environment: Rails.env).first
-      end
-
-      def payment_method
-        Spree::PaymentMethod.find(params[:payment_method])
-      end
-
-      def proccess_order
-        order = Spree::Order.by_number(params["order"]).first
-        raise(ActiveRecord::RecordNotFound) if order.nil?
-
-        unless order.payments.any?
-            order.payments.create!({
-            amount: order.total,
-            payment_method: payment_method
-          })
-          order.next
-        end
-
-        if order.complete?
-          flash[:success] = Spree.t(:order_mp_processed_successfully)
-          redirect_to order_path(order, token: order.token)
-        else
-          redirect_to checkout_state_path(order.state)
-        end
-      end
+      order
+    end
   end
 end
